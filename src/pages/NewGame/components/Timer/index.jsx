b8832173352ba72987291812cfc994e8fa757169
Andrea Paracino <andre.paracino@gmail.com>
2023-07-24 15:07:17 -0400

    feat: implement first version of Timer component

---
+++ src/pages/NewGame/components/Timer/index.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import { useState, useEffect, useRef } from "react";
+import { TIMER_DURATION } from "../../helpers/constants";
+
+const Timer = ({ onExpire }) => {
+  const [seconds, setSeconds] = useState(TIMER_DURATION);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setSeconds((prevSeconds) => prevSeconds - 0.1),
+      100
+    );
+    timerRef.current = intervalId;
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    if (seconds > 0) return;
+
+    onExpire();
+    clearInterval(timerRef.current);
+  }, [onExpire, seconds]);
+
+  return (
+    !!seconds && (
+      <div>
+        <h2>{Math.round(seconds * 10) / 10} seconds</h2>
+      </div>
+    )
+  );
+};
+
+Timer.propTypes = {
+  onExpire: PropTypes.func.isRequired,
+};
+
+export default Timer;
