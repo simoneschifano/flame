b2b5f8f7be10cf9aeb4030620a201508b92ca2a5
Marinavalenza <marina.valenza014@gmail.com>
2023-07-26 10:24:49 +0200

    feat: add logoand update scss constants

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,36 +1,26 @@
-import { getUsers, addUser } from "@/shared/helpers/api";
-import { getRandomQuestions } from "../NewGame/helpers/utilities";
-import Timer from "../NewGame/components/Timer";
+import styles from "../Landing/index.module.scss";
+import logo from "../../assets/brand/logo-word.png";
 
 const Landing = () => {
   return (
-    <div>
-      <button
-        onClick={async () => {
-          const questions = await getRandomQuestions("easy");
-          console.log(questions);
-        }}
-      >
-        get questions
-      </button>
-      <button
-        onClick={async () => {
-          const users = await getUsers();
-          console.log(users);
-        }}
-      >
-        get users
-      </button>
-      <button
-        onClick={async () => {
-          const user = await addUser("test2", 10);
-          console.log(user);
-        }}
-      >
-        add
-      </button>
-      <Timer onExpire={() => console.log("expired")} />
-    </div>
+    <main className={styles.Landing}>
+      <header className={styles["Landing-header"]}>
+        <img className={styles["Landing-logo"]} src={logo} alt="logo-word" />
+        <ul className={styles["Landing-infos"]}>
+          <li>About us</li>
+          <li>🏆</li>
+        </ul>
+      </header>
+      <p className={styles["Landing-instructions"]}>
+        <h2>The clock is ticking ⏰</h2>
+        Challenge your knowledge in this quiz game where time is essential! The
+        faster you choose the answer, higher your time bonus will be. There are
+        dozens of question categories and three levels of difficult. Keep track
+        of your results and your best scores! Challenge yourself to achieve the
+        highest score!
+      </p>
+      <button className={styles.newGameButton}>New Game</button>
+    </main>
   );
 };
 
