7ef7e99d83f013e488d1faab50621346339fd1db
Andrea Paracino <andre.paracino@gmail.com>
2023-07-30 13:34:33 -0400

    fix: lazy loading fix attempt

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import RoutesWrapper from "./setup/RoutesWrapper";
-import Loader from "./shared/components/Loader";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Suspense fallback={<Loader isContainerWide takesRemainingHeight />}>
-        <RoutesWrapper />
-      </Suspense>
+      <RoutesWrapper />
     </BrowserRouter>
   </React.StrictMode>
 );
